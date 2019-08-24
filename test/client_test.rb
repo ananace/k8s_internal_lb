3509da55323a3b337112319eb4fe608271cd451f
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-08-25 00:53:29 +0200

    Add more testing of client functionality

--- test/client_test.rb
+++ test/client_test.rb
@@ -31,22 +31,56 @@ class ClientTest < Test::Unit::TestCase
   def setup
     K8sInternalLb::Client.any_instance.expects(:in_cluster?).returns(false)
 
+    @kubeclient = mock
+
     @client = K8sInternalLb::Client.new
+    @client.stubs(:kubeclient).returns(@kubeclient)
   end
 
   def test_logger_existence
     refute_nil @client.send :logger
   end
 
-  def test_service_adding
+  def test_service_handling
     ep = mock
     ep.expects(:metadata).returns(nil)
-    @client.expects(:get_endpoint).returns(ep)
+    @kubeclient.expects(:get_endpoint).returns(ep)
 
     svc = K8sInternalLb::Services::TCP.new addresses: [], ports: [], name: 'test'
 
     @client.add_service svc
 
+    assert @client.services.key? 'test'
     assert_equal svc, @client.services['test']
+
+    @client.remove_service 'test'
+
+    refute @client.services.key? 'test'
+    refute_equal svc, @client.services['test']
+  end
+
+  def test_update
+    ep = mock
+    ep.expects(:metadata).returns(nil)
+    @kubeclient.expects(:get_endpoint).returns(ep)
+
+    svc = K8sInternalLb::Services::TCP.new addresses: [], ports: [], name: 'test', namespace: 'testing'
+    svc.expects(:update)
+    svc.expects(:endpoints).times(5)
+       .returns(
+         [
+           K8sInternalLb::Endpoint.new(address: K8sInternalLb::Address.new(ip: '1.2.3.4'), port: K8sInternalLb::Port.new(port: 1234), status: true),
+           K8sInternalLb::Endpoint.new(address: K8sInternalLb::Address.new(ip: '2.3.4.5'), port: K8sInternalLb::Port.new(port: 1234), status: false)
+         ]
+       )
+
+    now = Time.now
+    Time.stubs(:now).returns now
+
+    @kubeclient.expects(:patch_endpoint).with('test', { metadata: { annotations: { K8sInternalLb::Client::TIMESTAMP_ANNOTATION => now.to_i.to_s } }, subsets: svc.to_subsets }, 'testing').returns(svc)
+
+    @client.add_service svc
+
+    refute_equal true, @client.send(:update, svc, force: true)
   end
 end
