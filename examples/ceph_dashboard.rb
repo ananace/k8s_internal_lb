721bdc3d04079ac9e8835bbe93f8a461a610f36c
Alexander Olofsson <alexander.olofsson@liu.se>
2019-08-26 13:10:00 +0200

    Add an explanation to the ceph-dashboard example

--- examples/ceph_dashboard.rb
+++ examples/ceph_dashboard.rb
@@ -1,9 +1,38 @@
 # frozen_string_literal: true
 
 require 'k8s_internal_lb'
-require 'net/http'
-require 'resolv'
 
+# This example will generate Kubernetes endpoints that map to the Ceph
+# Dashboard service.
+#
+# Only the active manager serves the dashboard, all other nodes instead
+# generate 302 redirects to the currently active node.
+# This service will query all the given manager names, filter them based on
+# if they respond with HTTP 2xx or not, and fill in a Kubernetes endpoint
+# object like such;
+#
+# apiVersion: v1
+# kind: Endpoints
+# metadata:
+#   annotations:
+#     com.github.ananace.k8s-internal-lb/timestamp: 2019-08-26 10:57:47 +0000
+#   name: ceph-dashboard
+# subsets:
+# - addresses:
+#   - hostname: cephmgr2
+#     ip: 10.36.252.21
+#   notReadyAddresses:
+#   - hostname: cephmgr1
+#     ip: 10.36.252.20
+#   - hostname: cephmgr3
+#     ip: 10.36.252.22
+#   ports:
+#   - name: http
+#     port: 5000
+#     protocol: TCP
+#
+# At the moment, service objects have to be created beforehand.
+#
 class CephDashboard < K8sInternalLb::Services::HTTP
   attr_reader :mgrs
 
