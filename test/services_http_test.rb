194b6c62a99b744ecceb3cf9763c84dcbe974a21
Alexander Olofsson <alexander.olofsson@liu.se>
2019-08-26 11:28:52 +0200

    Add simple HTTP service test

---
+++ test/services_http_test.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ServicsHTTPTest < Test::Unit::TestCase
+  def test_creation
+    refute_nil K8sInternalLb::Services::HTTP.new name: 'test', addresses: ['http://example.com']
+    refute_nil K8sInternalLb::Services::HTTP.new name: 'test', addresses: ['http://example.com'], timeout: 2
+    refute_nil K8sInternalLb::Services::HTTP.new name: 'test', addresses: ['http://example.com'], method: :get, timeout: 2
+    refute_nil K8sInternalLb::Services::HTTP.new name: 'test', addresses: ['http://example.com'], method: :get, expects: 302, timeout: 2
+  end
+
+  def test_ports
+    svc = K8sInternalLb::Services::HTTP.new name: 'test', addresses: ['http://example.com']
+
+    assert_equal 80, svc.ports.first.port
+    assert_nil svc.ports.first.name
+
+    svc = K8sInternalLb::Services::HTTP.new name: 'test', addresses: ['http://example.com'], ports: [K8sInternalLb::Port.new(name: 'HTTP', port: 80)]
+
+    assert_equal 80, svc.ports.first.port
+    assert_equal 'HTTP', svc.ports.first.name
+  end
+end
